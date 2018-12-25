3c360ecdf56bc16e208ab999fade7febab229630
hyosunko <hyosunko@gmail.com>
2018-12-24 17:44:17 -0800

    Updates

---
+++ precache-manifest.4ec4f6a3ef22bbb44b8086f20044392d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68541af141f9d2aa2be2",
+    "url": "/treasure-hunt/static/js/runtime~main.68541af1.js"
+  },
+  {
+    "revision": "ff8b5f007f3f1b7e61cf",
+    "url": "/treasure-hunt/static/js/main.ff8b5f00.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/treasure-hunt/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ff8b5f007f3f1b7e61cf",
+    "url": "/treasure-hunt/static/css/main.07788dca.chunk.css"
+  },
+  {
+    "revision": "50a87e12841cbb813c5faef9abd9da92",
+    "url": "/treasure-hunt/index.html"
+  }
+];
